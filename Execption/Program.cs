a4d1e3f246fcd5421afa0d479a4c76f3235f5344
aayushhman <aayushmandal696@gmail.com>
2025-02-01 11:01:00 +0545

    handling execption

--- Execption/Program.cs
+++ Execption/Program.cs
@@ -10,6 +10,30 @@ namespace Execption
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter First number:");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Second number:");
+            int num2 = int.Parse(Console.ReadLine());
+
+            //int result = num1 / num2;
+            //Console.WriteLine("Division result is:" + result);
+            try
+            {
+                int result = num1 / num2;
+                Console.WriteLine("Division result is:" + result);
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine("you cannot devide a number by zero...");
+                Console.WriteLine(ex.Message);
+
+            }
+            Console.WriteLine("remaning statement 1"); 
+            Console.WriteLine("remaning statement 2");
+            Console.WriteLine("remaning statement 3");
+            Console.ReadLine();
+
         }
     }
 }
