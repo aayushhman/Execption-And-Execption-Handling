4242777b06a52b86628b8484bbf5e9e4eddf8a6c
aayushhman <aayushmandal696@gmail.com>
2025-02-01 20:51:51 +0545

    HANDLING INDEX OUT OF RANGE EXCEPTION IN CSHARP

--- Execption/Program.cs
+++ Execption/Program.cs
@@ -9,29 +9,63 @@ namespace Execption
     class Program
     {
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Enter First number:");
-            int num1 = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Enter Second number:");
-            int num2 = int.Parse(Console.ReadLine());
-
-            //int result = num1 / num2;
-            //Console.WriteLine("Division result is:" + result);
+            int[] a = new int[3];
             try
             {
-                int result = num1 / num2;
-                Console.WriteLine("Division result is:" + result);
-            }
-            catch(DivideByZeroException ex)
+            a[0] = 11;
+            a[1] = 22;
+            a[2] = 33;
+            a[3] = 44;
+
+               foreach (int item in a)
             {
-                Console.WriteLine("you cannot devide a number by zero...");
-                Console.WriteLine(ex.Message);
+                Console.WriteLine(item);
+            }
 
             }
-            Console.WriteLine("remaning statement 1"); 
-            Console.WriteLine("remaning statement 2");
-            Console.WriteLine("remaning statement 3");
+            catch(IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Array' range is excuded!!!");
+                Console.WriteLine(ex.Message);
+            }
+        
+          
+
+
+
+
+
+
+
+
+
+
+
+
+            //Console.WriteLine("Enter First number:");
+            //int num1 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter Second number:");
+            //int num2 = int.Parse(Console.ReadLine());
+
+            ////int result = num1 / num2;
+            ////Console.WriteLine("Division result is:" + result);
+            //try
+            //{
+            //    int result = num1 / num2;
+            //    Console.WriteLine("Division result is:" + result);
+            //}
+            //catch(DivideByZeroException ex)
+            //{
+            //    Console.WriteLine("you cannot devide a number by zero...");
+            //    Console.WriteLine(ex.Message);
+
+            //}
+            //Console.WriteLine("remaning statement 1"); 
+            //Console.WriteLine("remaning statement 2");
+            //Console.WriteLine("remaning statement 3");
             Console.ReadLine();
 
         }
